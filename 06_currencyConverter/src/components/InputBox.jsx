29b098728adf7045bd5b5d4c8b04ba9825fc1d88
Tim-arp <baburegmi100@gmail.com>
2026-01-18 21:21:06 +0545

    Currency Converter

--- 06_currencyConverter/src/components/InputBox.jsx
+++ 06_currencyConverter/src/components/InputBox.jsx
@@ -29,6 +29,7 @@ function InputBox({
                     onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
+            {/* dd  */}
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
