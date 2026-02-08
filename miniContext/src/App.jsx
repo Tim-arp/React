4e6f54387146e2b4d65607b9e30ab6e26da961c8
Tim-arp <baburegmi100@gmail.com>
2026-02-08 23:33:49 +0545

    Theme Changer

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
-import Profile from './components/Profile'
-import UserContextProvider from './context/UserContextProvider'
 import Login from './components/Login'
+import Profile from './components/Profile'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+
 function App() {
+
   return (
     <UserContextProvider>
-      <h1>Context API</h1>
       <Login/>
       <Profile/>
     </UserContextProvider>
--- miniContext/src/components/Login.jsx
+++ miniContext/src/components/Login.jsx
@@ -1,7 +1,7 @@
-import React, {useState, useContext} from 'react'
+import React, { useContext, useState } from 'react'
 import UserContext from '../context/UserContext'
-function Login() {
 
+function Login() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
@@ -9,24 +9,27 @@ function Login() {
 
     const handleSubmit = (e)=>{
         e.preventDefault()
-        setUser({username, password})
+        setUser(username,password)
     }
-  return (
-    <div>
-        <h2>Login</h2>
-        <input type="text" 
-        value={username}
-        onChange={(e)=>setUsername(e.target.value)}
-        placeholder='username' />
 
-        <input type="text"
-        value={password}
-        onChange={(e)=>setPassword(e.target.value)}
-        placeholder='password' />
-
-        <button onClick={handleSubmit}>Submit</button>
-    </div>
-  )
+    return (
+        <>
+            <h2>Login</h2>
+            <input type="text"
+                placeholder='Username'
+                value={username}
+                onChange={ (e)=>{setUsername(e.target.value) }}
+            />
+            <input type="text"
+                placeholder='Password'
+                value={password}
+                onChange={ (e)=>{setPassword(e.target.value) }}
+            />
+            <button type="Submit"
+            onClick={handleSubmit}
+            >Submit</button>
+        </>
+    )
 }
 
 export default Login
